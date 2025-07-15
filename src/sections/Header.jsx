59b8e07dca36211dbee084d5ffe7f749407b8694
ToufikIIT <toufik.haque2021@gmail.com>
2025-07-15 13:35:31 +0530

    Hero,Features

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -35,8 +35,8 @@ const Header = () => {
   return (
     <header
       className={clsx(
-        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
-        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",
+        "fixed top-0 left-0 z-50 w-full py-8 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-1 bg-black-100 backdrop-blur-[8px]",
       )}
     >
       <div className="container flex h-14 items-center max-lg:px-5">
